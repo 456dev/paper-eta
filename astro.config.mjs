be37edd44dfa225e0ecd514f8ed4632fe7aa9623
Isaac - The456 <the456@the456gamer.dev>
2025-04-15 01:29:18 +0100

    fix urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
+// CF_PAGES_URL
+const deployment_target = process.env.CF_PAGES_URL || "https://paper-eta.0456456.xyz"
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+  site: deployment_target
 });
